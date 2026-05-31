6c615314560fa81321d9ace0fb042de7d7443dbc
Sorrawee Worawichayawiwat <sorrawee@mongkol.net>
2026-05-31 23:50:11 +0700

    Add jar

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar --module-path "C:\Users\Kaka\Downloads\openjfx-24.0.1_windows-x64_bin-sdk\javafx-sdk-24.0.1\lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media Final_Project-1-with-sources.jar
+pause
